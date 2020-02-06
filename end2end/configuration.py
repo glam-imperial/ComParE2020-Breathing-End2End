dce9c169697a43c3f2b1e9e0d8d6d6b5aa91a540
Georgios Rizos <georgevrizos@gmail.com>
2020-02-06 11:42:12 +0000

    Add files via upload

---
+++ end2end/configuration.py
@@ -0,0 +1,7 @@
+# CHALLENGE_FOLDER = "/path/to/challenge/folder"
+# TF_RECORDS_FOLDER = CHALLENGE_FOLDER + "/tfrecords"
+# OUTPUT_FOLDER = "/data/Data/UCL_SBM/challenge/output"
+
+CHALLENGE_FOLDER = "/data/PycharmProjects/ComParE2020-Breathing-End2End"
+TF_RECORDS_FOLDER = "/data/PycharmProjects/ComParE2020-Breathing-End2End/tf_records"
+OUTPUT_FOLDER = "/data/PycharmProjects/ComParE2020-Breathing-End2End/output"
